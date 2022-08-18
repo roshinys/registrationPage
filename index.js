8593a859fbc0423d0259d59c8cb7ea5dbf408abe
roshin Y S <roshinchaya@gmail.com>
2022-08-18 10:16:45 +0530

    localStorage V2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("working");
+var form = document.querySelector(".form-body");
+
+// console.log(form);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var email = e.target.name.value;
+  var name = e.target.email.value;
+  // console.log(name, email);
+  localStorage.setItem("name", name);
+  localStorage.setItem("email", email);
+});
