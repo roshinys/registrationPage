6d6c52b266ad4b3ea74b7e5e2f8169ad0c9e7cbb
roshin Y S <roshinchaya@gmail.com>
2022-09-01 13:36:09 +0530

    crud v3

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 //using axios and crud
 var form = document.querySelector(".form-body");
-window.addEventListener("DOMContentLoaded", () => {
-  axios
+
+//get all users
+window.addEventListener("DOMContentLoaded", async () => {
+  await axios
     .get(
-      "https://crudcrud.com/api/ed73d95fff724af19de41a39f5d5e8ea/userDetails"
+      "https://crudcrud.com/api/e057dba6b15f411e81bfef5997c43291/userDetails"
     )
     .then((res) => {
       const users = res.data;
       console.log(users);
       users.forEach((user) => {
-        // console.log(user);
         showUserInFrontEnd(user);
       });
     })
@@ -17,6 +18,8 @@ window.addEventListener("DOMContentLoaded", () => {
       console.log(err);
     });
 });
+
+//create users
 
 var submitForm = (event) => {
   event.preventDefault();
@@ -28,7 +31,7 @@ var submitForm = (event) => {
   };
   axios
     .post(
-      "https://crudcrud.com/api/ed73d95fff724af19de41a39f5d5e8ea/userDetails",
+      "https://crudcrud.com/api/e057dba6b15f411e81bfef5997c43291/userDetails",
       user
     )
     .then((res) => {
@@ -42,22 +45,32 @@ var submitForm = (event) => {
 
 function showUserInFrontEnd(user) {
   // console.log(user);
-  var parentNode = document.getElementById("items");
-  var child = document.createElement("li");
-  child.innerText = `${user.email} ${user.name}`;
-  var btn = document.createElement("button");
-  btn.className = "mg-5";
-  btn.className = "delete";
-  btn.innerText = "delete";
-  btn.value = `${user.email}`;
-  var editbtn = document.createElement("button");
-  editbtn.className = "mg-5";
-  editbtn.className = "edit";
-  editbtn.innerText = "edit";
-  editbtn.value = `${user.email}`;
-  child.append(editbtn);
-  child.appendChild(btn);
-  parentNode.appendChild(child);
+  const parentNode = document.getElementById("listofusers");
+  const childHtml = `<li  id=${user._id} >${user.name} - ${user.email} <button onclick=deleteUser("${user._id}")>DeleteUser</button></li>`;
+  parentNode.innerHTML = parentNode.innerHTML + childHtml;
+}
+
+//delete users;
+function deleteUser(userId) {
+  axios
+    .delete(
+      `https://crudcrud.com/api/e057dba6b15f411e81bfef5997c43291/userDetails/${userId}`
+    )
+    .then((res) => {
+      console.log("successfully deleted user with id " + userId);
+      removeUserFromScreen(userId);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function removeUserFromScreen(userId) {
+  const parentNode = document.getElementById("listofusers");
+  const childNode = document.getElementById(userId);
+  if (childNode) {
+    parentNode.removeChild(childNode);
+  }
 }
 
 //using localStorage
